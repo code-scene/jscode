b4e66a7d737902bee960ef5e8c6e233b07c0d363
code-scene <117392017+code-scene@users.noreply.github.com>
2023-10-31 02:09:15 +0900

    Update support-mode.js
    function の指定を忘れてた
    入れた

--- support-mode.js
+++ support-mode.js
@@ -1,5 +1,5 @@
 
-run(){
+function run(){
 	var l_uri = leftUsrAttr.location.href;
 	var uID = location.href.match(/usrID=\d+/)[0].match(/\d+/)[0];
 	leftUsrAttr.location.href = l_uri.replace(/\.php\?.+?$/i, '.php?usrID=' + uID + '&supportFlg=on');
