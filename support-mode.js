7656003fb98c98f1543763c228a0e9586eddcaf0
code-scene <117392017+code-scene@users.noreply.github.com>
2023-10-31 01:45:51 +0900

    Create support-mode.js
    first

---
+++ support-mode.js
@@ -0,0 +1,6 @@
+
+run(){
+	var l_uri = leftUsrAttr.location.href;
+	var uID = location.href.match(/usrID=\d+/)[0].match(/\d+/)[0];
+	leftUsrAttr.location.href = l_uri.replace(/\.php\?.+?$/i, '.php?usrID=' + uID + '&supportFlg=on');
+}
