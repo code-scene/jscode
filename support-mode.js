6cb4dabf6af0c55d052fa58ac3e31447474a7f5e
code-scene <117392017+code-scene@users.noreply.github.com>
2023-10-31 02:14:51 +0900

    Update support-mode.js
    関数の外に出した

--- support-mode.js
+++ support-mode.js
@@ -1,6 +1,5 @@
 
-function run(){
-	var l_uri = leftUsrAttr.location.href;
-	var uID = location.href.match(/usrID=\d+/)[0].match(/\d+/)[0];
-	leftUsrAttr.location.href = l_uri.replace(/\.php\?.+?$/i, '.php?usrID=' + uID + '&supportFlg=on');
-}
+var l_uri = leftUsrAttr.location.href;
+var uID = location.href.match(/usrID=\d+/)[0].match(/\d+/)[0];
+leftUsrAttr.location.href = l_uri.replace(/\.php\?.+?$/i, '.php?usrID=' + uID + '&supportFlg=on');
+
